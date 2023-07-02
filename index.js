581a4207d7fc87c5f34379d32f40df4d7e5b5b16
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-07-02 00:31:51 -0300

    Atualizando
    Nao aprendi quase nada

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const app = express()
 
+pessoa = [
+    {
+        nome: "Guilherme",
+        idade: 20,
+        profissao: "Auxiliar de Legalizacao"
+    },
+    {
+        nome: "Paulo",
+        idade: 21,
+        profissao: "Vagabundo"
+    }
+]
+
 app.get('/', (request, response) => {
     return response.json({
-        message: "Testando, acho que melhorei um pouco. Agora tudo se atualiza em tempo real!"
+        message: "Testando, acho que melhorei um pouco. Agora tudo se atualiza em tempo real!\nAqui aparece no navegador. Mudei ein"
+    })
+})
+
+app.post('/object', (request, response)=>{
+    return response.json({
+        message: "testing the program"
     })
 })
 
 app.listen(3000, () => {
-    console.log("Testando aplicacao")
+    console.log("Aqui aparece no Terminal")
 })
