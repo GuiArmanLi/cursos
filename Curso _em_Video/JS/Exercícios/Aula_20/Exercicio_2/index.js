58765ffb716f8f9315b7b99765c389442f121df0
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-09-22 18:06:24 -0300

    Concluído

---
+++ Curso _em_Video/JS/Exercícios/Aula_20/Exercicio_2/index.js
@@ -0,0 +1,25 @@
+number = document.getElementById("number");
+btn = document.getElementById("btn");
+table = document.getElementById("table");
+
+btn.addEventListener("click", () => {
+    try {
+        table.innerHTML = "";
+        if (number.value === "") {
+            alert("Ops. Algo deu errado. Verifique se o valor está correto!")
+        } else {
+            gerarTabuada()
+        }
+    } catch (error) { alert(error) } finally { number.value = ""; }
+})
+
+function gerarTabuada() {
+    _number = Number(number.value)
+    let res = 0;
+    let options = "";
+    for (let i = 0; i <= 10; i++) {
+        res = _number * i;
+        options += `<option>${_number} x ${i} = ${res}</option>`
+    }
+    table.innerHTML = `<optgroup>${options}</optgroup>`
+}
