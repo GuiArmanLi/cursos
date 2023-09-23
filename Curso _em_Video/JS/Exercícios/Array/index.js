e0f1519ed28907580cd23cbc877383a831dcdf29
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-09-22 21:35:11 -0300

    Concluído

---
+++ Curso _em_Video/JS/Exercícios/Array/index.js
@@ -0,0 +1,45 @@
+input = document.getElementById("input");
+btnAdd = document.getElementById("btnAdd");
+list = document.getElementById("list");
+btnCalcular = document.getElementById("btn-calcular");
+contentResponse = document.getElementById("content-response");
+select = document.getElementById("select");
+
+
+array = []
+
+btnAdd.addEventListener("click", () => {
+    try {
+        let display = "flex"
+        let item = Number(input.value)
+        item != 0 ? array.push(item) : display = "none";
+
+        select.innerHTML = createSelectArray()
+        list.style.display = display;
+    } catch (error) { alert(error) } finally { input.value = "", contentResponse.style.display = "none" }
+})
+
+btnCalcular.addEventListener("click", () => {
+    contentResponse.style.display = "flex"
+    manipularArray()
+})
+
+function createSelectArray() {
+    let itens = "";
+    for (let i = 0; i < array.length; i++) { itens += `<option>${array[i]}</option>` }
+    return itens;
+}
+
+function manipularArray() {
+    let soma = 0;
+    for (let i = 0; i < array.length; i++) { soma += array[i]; }
+    let media = soma / array.length;
+    let arraySort = array.sort();
+
+    contentResponse.innerHTML =
+        `<p>Ao todo temos ${array.length} valores cadastrados.</p>
+    <p>O maior valor informado foi ${arraySort[arraySort.length - 1]}.</p>
+    <p>O menor valor informado foi ${arraySort[0]}.</p>
+    <p>Somando todos os valores temos ${soma}.</p>
+    <p>A média dos valores digitados é ${Math.floor(media)}.</p>`
+}
